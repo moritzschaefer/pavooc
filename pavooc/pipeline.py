286080ee708b807c22700c878dff849362f209f8
Moritz <mail@moritzs.de>
2017-09-27 14:14:47 +0200

    add index to mongodb

--- pavooc/pipeline.py
+++ pavooc/pipeline.py
@@ -3,8 +3,10 @@ ipdb.set_trace()
 from pavooc.data_integration.downloader import main as main_downloader
 from pavooc.preprocessing.preprocessing import main as main_preprocessing
 from pavooc.preprocessing.prepare_flashfry import main as main_ff
+from pavooc.preprocessing.sgrna_finder import main as main_sgrna
 
 if __name__ == "__main__":
     main_downloader()
     main_preprocessing()
     main_ff()
+    main_sgrna()
