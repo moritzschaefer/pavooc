9ab420a2fca25ffe5efbc20bc129dd76ad498259
Moritz <mail@moritzs.de>
2017-09-27 14:11:02 +0200

    add util.py

---
+++ pavooc/util.py
@@ -0,0 +1,27 @@
+
+def kmer_to_int(kmer):
+    '''Transform a 20bp DNA sequence to a 64 bit integer'''
+    code = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+
+    if len(kmer) != 20:
+        raise ValueError('Only 20mers supported')
+
+    v = 0
+    for i, c in enumerate(kmer):
+        v <<= 2
+        v += code[c]
+
+    return v
+
+
+def int_to_kmer(v):
+    '''Transform a 64 bit integer to a 20mer DNA sequence'''
+    code = ['A', 'C', 'G', 'T']
+
+    kmer = ''
+
+    for i in range(20):
+        kmer = code[v & 3] + kmer
+        v >>= 2
+
+    return kmer
