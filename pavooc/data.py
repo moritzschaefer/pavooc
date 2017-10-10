afd9a53b4127febadc6d6b06aeab8f8f4324a66a
Moritz <mail@moritzs.de>
2017-10-10 11:33:20 +0200

    refactor return value buffering

--- pavooc/data.py
+++ pavooc/data.py
@@ -5,23 +5,18 @@ import os
 import pickle
 
 from gtfparse import read_gtf_as_dataframe
-import pandas as pd
 import azimuth
 
 from pavooc.config import GENCODE_FILE, CHROMOSOMES, CHROMOSOME_RAW_FILE
 from pavooc.util import buffer_return_value
 
 
+@buffer_return_value
 def read_gencode():
     '''
     Buffered gencode
     '''
-    if not isinstance(read_gencode.buffered, pd.DataFrame):
-        read_gencode.buffered = read_gtf_as_dataframe(GENCODE_FILE)
-    return read_gencode.buffered
-
-
-read_gencode.buffered = None
+    return read_gtf_as_dataframe(GENCODE_FILE)
 
 
 # TODO annotation to buffer output automatically
@@ -48,37 +43,26 @@ def gencode_exons_gene_grouped():
     return gencode.groupby('gene_id')
 
 
+@buffer_return_value
 def chromosomes():
     '''
     Return dictionary with loaded chromosome data
     '''
-    if not chromosomes.buffered:
-        chromosomes.buffered = {
-                c: open(CHROMOSOME_RAW_FILE.format(c)).read()
-                for c in CHROMOSOMES}
-    return chromosomes.buffered
+    return {
+            c: open(CHROMOSOME_RAW_FILE.format(c)).read()
+            for c in CHROMOSOMES}
 
 
-chromosomes.buffered = None
-
-
+@buffer_return_value
 def azimuth_model(nopos=True):
-    import ipdb
-    ipdb.set_trace()
-    if not azimuth_model.buffered:
-        azimuth_saved_model_dir = os.path.join(
-                os.path.dirname(azimuth.__file__),
-                'saved_models')
-        if nopos:
-            model_name = 'V3_model_nopos.pickle'
-        else:
-            model_name = 'V3_model_full.pickle'
+    azimuth_saved_model_dir = os.path.join(
+            os.path.dirname(azimuth.__file__),
+            'saved_models')
+    if nopos:
+        model_name = 'V3_model_nopos.pickle'
+    else:
+        model_name = 'V3_model_full.pickle'
 
-        model_path = os.path.join(azimuth_saved_model_dir, model_name)
-        with open(model_path, 'rb') as f:
-            azimuth_model.buffered = pickle.load(f)
-
-    return azimuth_model.buffered
-
-
-azimuth_model.buffered = None
+    model_path = os.path.join(azimuth_saved_model_dir, model_name)
+    with open(model_path, 'rb') as f:
+        return pickle.load(f)
