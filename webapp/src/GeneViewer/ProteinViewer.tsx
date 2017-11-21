4d6721d5d796f011603a7f52a649090be7b65d76
Moritz <mail@moritzs.de>
2017-11-21 14:10:57 +0100

    proper hovering and coloring for ProteinViewer

--- webapp/src/GeneViewer/ProteinViewer.tsx
+++ webapp/src/GeneViewer/ProteinViewer.tsx
@@ -41,16 +41,20 @@ export default class ProteinViewer extends React.Component<Props, State> {
     if (hoveredGuide) {
       // TODO guides[hovered] is undefined sometimes
       const aa_cut_position = guides[hoveredGuide].aa_cut_position;
-      const highlightPosition = aa_cut_position - (pdb.SP_BEG);
-      if (aa_cut_position >= 0 && highlightPosition ) {
-        selection = `${highlightPosition-1}-${highlightPosition + 2}`; // bugfix extending indices
+      const highlightPosition = aa_cut_position - pdb.SP_BEG;
+      if (aa_cut_position >= 0 && highlightPosition) {
+        selection = `${highlightPosition - 1}-${highlightPosition + 2}`; // bugfix extending indices
       }
     } else {
       const cuts = [];
       for (let guide of guides) {
-        const inPdbPosition = guide.aa_cut_position - (pdb.SP_BEG);
-        if (guide.aa_cut_position >= 0 && inPdbPosition >= 0 && inPdbPosition < pdb.SP_END - pdb.SP_BEG) {
-          cuts.push(`${inPdbPosition-1}-${inPdbPosition+2}`); // bugfix extending indices
+        const inPdbPosition = guide.aa_cut_position - pdb.SP_BEG;
+        if (
+          guide.aa_cut_position >= 0 &&
+          inPdbPosition >= 0 &&
+          inPdbPosition < pdb.SP_END - pdb.SP_BEG
+        ) {
+          cuts.push(`${inPdbPosition - 1}-${inPdbPosition + 2}`); // bugfix extending indices
         }
       }
       if (cuts.length === 0) {
@@ -62,32 +66,52 @@ export default class ProteinViewer extends React.Component<Props, State> {
     representation.setSelection(selection);
   }
 
-  loadPdb() {
-    const { pdb, guides } = this.props;
-    const { stage } = this.state;
-    const { props } = this;
-
-
-    // find and highlight cut positions
-    stage.removeAllComponents();
-
-    let scheme = NGL.ColormakerRegistry.addScheme( function(this: any, params: any ) {
-      this.atomColor = function( atom: any ) {
+  generateScheme() {
+    const { hoveredGuide, guides, pdb } = this.props;
+    let scheme = NGL.ColormakerRegistry.addScheme(function(
+      this: any,
+      params: any
+    ) {
+      this.atomColor = function(atom: any) {
         // the residue index is zero-based, same order as in the loaded file
         // TODO cache atom.residue.index (because it is slow!) Map<atom, atom.residue.index>
         let res = atom.residue.index;
-        if (props.hoveredGuide) {
-          if (guides[props.hoveredGuide].aa_cut_position - pdb.SP_BEG === res) {
-            return 0xFFFF00;
+        if (hoveredGuide) {
+          if (guides[hoveredGuide].aa_cut_position - pdb.SP_BEG === res) {
+            if (guides[hoveredGuide].selected) {
+              return 0xffff00;
+            } else {
+              return 0xff0000;
+            }
+          } else {
+            return 0x777777;
           }
         }
         // TODO Map<aa_cut_position-pdb.SP_BEG, guide> for fast lookup
-        if (guides.find((guide: any) => guide.aa_cut_position - pdb.SP_BEG === res)) {
-          return 0xFF0000;
+        const guide = guides.find(
+          (g: any) => g.aa_cut_position - pdb.SP_BEG === res
+        );
+        if (guide) {
+          if (guide.selected) {
+            return 0xbbbb00;
+          } else {
+            return 0x990000;
+          }
+        } else {
+          return 0x999999;
         }
-        return 0x777777;
       };
-    } );
+    });
+    return scheme;
+  }
+
+  loadPdb() {
+    const { pdb } = this.props;
+    const { stage } = this.state;
+
+    // find and highlight cut positions
+    stage.removeAllComponents();
+    let scheme = this.generateScheme();
 
     stage
       .loadFile(`rcsb://${pdb.PDB}`)
@@ -103,18 +127,19 @@ export default class ProteinViewer extends React.Component<Props, State> {
 
   componentDidUpdate(prevProps: Props, prevState: State) {
     if (this.state.stage) {
+      const selectionChanged = false; // TODO implement
       if (
         prevProps.pdb.PDB !== this.props.pdb.PDB ||
         (prevState.stage !== this.state.stage && this.state.stage)
       ) {
         this.loadPdb();
-      } else if (prevProps.hoveredGuide !== this.props.hoveredGuide) {
-        this.updateHovering();
-      }
+      } else if (
+        this.state.representation &&
+        (prevProps.hoveredGuide !== this.props.hoveredGuide || selectionChanged)
+      ) {
+        this.state.representation.setColor(this.generateScheme());
 
-      // initial Highlighting
-      if (prevState.representation !== this.state.representation) {
-        this.updateHovering();
+        //this.state.representation.update({color: true})
       }
     } else {
       console.log("Error: Stage is not loaded...");
@@ -127,14 +152,13 @@ export default class ProteinViewer extends React.Component<Props, State> {
     const stage = new NGL.Stage(viewport);
     stage.setParameters({ backgroundColor: "black" });
     // listen to `hovered` signal to move tooltip around and change its text
-    stage.signals.hovered.add((pickingProxy: any) =>  {
+    stage.signals.hovered.add((pickingProxy: any) => {
       if (pickingProxy && (pickingProxy.atom || pickingProxy.bond)) {
-        // TODO find corresponding guide!!
-        // setHoveredGuide(pickingProxy.pid);
         const res = pickingProxy.atom.residue.index;
-        // TODO fixme, not the correct position.. (or other positions suck..)
-        const guideIndex = guides.findIndex((guide: any) => res === guide.aa_cut_position - pdb.SP_BEG);
-        if (guideIndex) {
+        const guideIndex = guides.findIndex(
+          (guide: any) => res === guide.aa_cut_position - pdb.SP_BEG
+        );
+        if (guideIndex >= 0) {
           setHoveredGuide(guideIndex);
         }
       } else {
