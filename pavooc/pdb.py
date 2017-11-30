11caa096084a8c01402c0504441f0fb45a25b2bb
Moritz <mail@moritzs.de>
2017-11-30 18:50:55 +0100

    add init files and pdb.py

---
+++ pavooc/pdb.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import xml.etree.ElementTree as ET
+import re
+from gzip import GzipFile
+
+from pavooc.config import SIFTS_FILE
+
+
+def pdb_mappings(pdb, chain, swissprot_id):
+    intre = re.compile('-?\d+')
+    fi = GzipFile(SIFTS_FILE.format(
+        '{}.xml.gz'.format(pdb.lower()), 'r'))
+    # parse xml
+    ret = {}
+    tree = ET.fromstring(fi.read())
+    for i in range(len(tree)):
+        if tree[i].tag.split('}')[-1] != 'entity':
+            continue
+        for j in range(len(tree[i])):
+            if tree[i][j].tag.split('}')[-1] != 'segment':
+                continue
+            for k in range(len(tree[i][j])):
+                if tree[i][j][k].tag.split('}')[-1] != 'listResidue':
+                    continue
+                for resid in tree[i][j][k].getchildren():
+                    pdb_id = None
+                    pdb_rn = None  # residue number
+                    pdb_aa = None
+                    uniprot_id = None
+                    uniprot_rn = None  # residue number
+                    uniprot_aa = None
+                    for ref in resid.getchildren():
+                        if ref.tag.split('}')[-1] != "crossRefDb":
+                            continue
+                        if ref.attrib['dbSource'] == 'PDB':
+                            if chain != ref.attrib['dbChainId']:
+                                continue
+                            pdb_id = ref.attrib['dbAccessionId']
+                            pdb_chain = ref.attrib['dbChainId']
+                            pdb_aa = ref.attrib['dbResName']
+                            try:
+                                pdb_rn = int(intre.findall(
+                                    ref.attrib['dbResNum'])[0])
+                            except IndexError:
+                                pass
+
+                        elif ref.attrib['dbSource'] == 'UniProt':
+                            if swissprot_id != ref.attrib['dbAccessionId']:
+                                continue
+                            uniprot_id = ref.attrib['dbAccessionId']
+                            uniprot_aa = ref.attrib['dbResName']
+                            uniprot_rn = int(ref.attrib['dbResNum'])
+                    if pdb_rn is not None and uniprot_rn is not None:
+                        # should not happen; otherwise: redundant maps
+                        if uniprot_rn in ret and ret[uniprot_rn] != pdb_rn:
+                            print(pdb, chain,
+                                  swissprot_id,
+                                  ret[uniprot_rn], (pdb_rn, pdb_aa))
+                            #raise Exception("Fix this! 1")
+                        #ret[uniprot_rn] = (pdb_rn, pdb_aa)
+                        ret[uniprot_rn] = pdb_rn
+    return ret
