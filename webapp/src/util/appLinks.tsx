351dd6c8892209169cd22a6cbb20b020fb54d611
Moritz Schaefer <mail@moritzs.de>
2019-06-17 18:23:22 +0200

    2018->2019

--- webapp/src/util/appLinks.tsx
+++ webapp/src/util/appLinks.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export const renderAppLinks = (className: string) => {
     return (
         <div {...{ className }} >
-            <a target="_blank" href="http://pavooc.io/api">API</a> | <a target="_blank" href="https://www.youtube.com/watch?v=XDwK73LI9Vk">Tutorial</a> | <a target="_blank" href="https://github.com/moritzschaefer/pavooc/">GitHub</a> | &copy; 2018 <a href="http://moritzs.de/blog">Moritz Schaefer</a>
+            <a target="_blank" href="http://pavooc.io/api">API</a> | <a target="_blank" href="https://www.youtube.com/watch?v=XDwK73LI9Vk">Tutorial</a> | <a target="_blank" href="https://github.com/moritzschaefer/pavooc/">GitHub</a> | &copy; {new Date().getFullYear()} <a href="http://moritzs.de/blog">Moritz Schaefer</a>
         </div>
     );
 }
