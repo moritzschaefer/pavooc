8b73e5bdfe6dcb1dfa446ed9c9e052d75e261711
Moritz Schaefer <mail@moritzs.de>
2020-04-06 01:38:15 +0200

    add publication link

--- webapp/src/util/appLinks.tsx
+++ webapp/src/util/appLinks.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const renderAppLinks = (className: string) => {
     return (
         <div {...{ className }} >
-            <a target="_blank" href="http://pavooc.io/api">API</a> | <a target="_blank" href="https://www.youtube.com/watch?v=XDwK73LI9Vk">Tutorial</a> | <a target="_blank" href="https://github.com/moritzschaefer/pavooc/">GitHub</a> | <Link to="/impressum">Impressum</Link> | &copy; {new Date().getFullYear()} <a href="http://moritzs.de/blog">Moritz Schaefer</a>
+            <a target="_blank" href="https://academic.oup.com/bioinformatics/article/35/13/2309/5184963">Publication</a> | <a target="_blank" href="https://www.youtube.com/watch?v=XDwK73LI9Vk">Tutorial</a> | <a target="_blank" href="https://github.com/moritzschaefer/pavooc/">GitHub</a> | <Link to="/impressum">Impressum</Link> | &copy; {new Date().getFullYear()} <a href="http://moritzs.de/blog">Moritz Schaefer</a>
         </div>
     );
 }
